d4fc72042f2d5df71fbc80a22543c644e3190d43
seinokoula <alexandre.charles92@gmail.com>
2023-07-05 13:55:51 +0200

    dsqcxcwcxw

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use Achar\PakajeDemo\Api;
+use gokilp\PakajeDemo\Api;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends \PHPUnit\Framework\TestCase
 {
     public function testGetRandomNumber()
     {
-        $api = new \Achar\PakajeDemo\Api();
+        $api = new \gokilp\PakajeDemo\Api();
         $this->assertIsInt($api->getRandomNumber());
         $this->assertGreaterThanOrEqual(0, $api->getRandomNumber());
         $this->assertLessThanOrEqual(100, $api->getRandomNumber());
